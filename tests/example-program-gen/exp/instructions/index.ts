59a44e92ba3239da113d50ae901aa2be2cee50ee
Krešimir Klas <github@kklas.net>
2022-04-10 14:49:25 +0000

    add test for nested fields

--- tests/example-program-gen/exp/instructions/index.ts
+++ tests/example-program-gen/exp/instructions/index.ts
@@ -4,4 +4,9 @@ export {
   InitializeWithValuesArgs,
   InitializeWithValuesAccounts,
 } from "./initializeWithValues"
+export {
+  initializeWithValues2,
+  InitializeWithValues2Args,
+  InitializeWithValues2Accounts,
+} from "./initializeWithValues2"
 export { causeError } from "./causeError"
